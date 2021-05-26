cc221dce852dffa57983e72f1866b5a4fd3ab23d
LorD-222 <ilya.r.200594@gmail.com>
2021-05-26 11:00:05 +0300

    work with ftp

---
+++ Ftp to csv.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\PuTTY\psftp.exe" -v -C -b "D:\DB77\Vygr\Ftp to csv.txt" -2 login@server -P 22 -pw "Pass"
+:: "D:\DB77\Vygr\Ftp to csv.txt" - путь файла с командами
+:: Подключение к серверу, формат login@server, -P порт, -pw "Pass". 
+
---
+++ Merge to ftp.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\PuTTY\psftp.exe" -v -C -b "D:\DB77\Vygr\Merge to ftp.txt" -2 login@server -P 22 -pw "Pass"
+:: "D:\DB77\Vygr\Ftp to csv.txt" - путь файла с командами
+:: Подключение к серверу, формат login@server, -P порт, -pw "Pass". 
+
+
